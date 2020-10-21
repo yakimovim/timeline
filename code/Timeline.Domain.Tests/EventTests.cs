b7f1a543acefd348b9150cba6b4484a90566742b
Ivan Iakimov <edlinsoft@gmail.com>
2020-10-21 11:58:51 +0300

    Event

---
+++ code/Timeline.Domain.Tests/EventTests.cs
@@ -0,0 +1,87 @@
+﻿using Shouldly;
+using System;
+using Xunit;
+
+namespace EdlinSoftware.Timeline.Domain.Tests
+{
+    public class EventTests
+    {
+        [Fact]
+        public void Cant_create_event_without_start()
+        {
+            Should.Throw<ArgumentNullException>(() =>
+            {
+                new Event<string>("Event", null);
+            });
+        }
+
+        [Fact]
+        public void Cant_create_event_with_start_greater_then_end()
+        {
+            Should.Throw<ArgumentException>(() =>
+            {
+                new Event<string>("Event", SpecificDate.AnnoDomini(2020), SpecificDate.AnnoDomini(2000));
+            });
+        }
+
+        [Fact]
+        public void Cant_set_null_start()
+        {
+            var @event = new Event<string>(
+                "Event", 
+                SpecificDate.AnnoDomini(2000), 
+                SpecificDate.AnnoDomini(2020)
+            );
+
+            Should.Throw<ArgumentException>(() =>
+            {
+                @event.SetInterval(null);
+            });
+        }
+
+        [Fact]
+        public void Cant_set_start_greater_then_end()
+        {
+            var @event = new Event<string>(
+                "Event",
+                SpecificDate.AnnoDomini(2000),
+                SpecificDate.AnnoDomini(2020)
+            );
+
+            Should.Throw<ArgumentException>(() =>
+            {
+                @event.SetInterval(
+                    SpecificDate.AnnoDomini(2020),
+                    SpecificDate.AnnoDomini(2000)
+                );
+            });
+        }
+
+        [Fact]
+        public void Event_with_start_and_end_has_correct_date_related_properties()
+        {
+            var @event = new Event<string>(
+                "Event",
+                SpecificDate.AnnoDomini(2000),
+                SpecificDate.AnnoDomini(2020)
+            );
+
+            @event.Start.ShouldBe(SpecificDate.AnnoDomini(2000));
+            @event.End.ShouldBe(SpecificDate.AnnoDomini(2020));
+            @event.Duration.ShouldBe(Duration.Zero.AddYears(20));
+        }
+
+        [Fact]
+        public void Event_with_only_start_has_correct_date_related_properties()
+        {
+            var @event = new Event<string>(
+                "Event",
+                SpecificDate.AnnoDomini(2000)
+            );
+
+            @event.Start.ShouldBe(SpecificDate.AnnoDomini(2000));
+            @event.End.ShouldBeNull();
+            @event.Duration.ShouldBe(Duration.Zero);
+        }
+    }
+}
--- code/src/Timeline.Domain/Event.cs
+++ code/src/Timeline.Domain/Event.cs
@@ -8,12 +8,12 @@ namespace EdlinSoftware.Timeline.Domain
         private TDescription _description;
         private Date _start;
 
-        public Event(TDescription description, Date start)
+        public Event(TDescription description, Date start, Date end = null)
         {
             if(description == null)
                 throw new ArgumentNullException(nameof(description), "Event description can't be null.");
             _description = description;
-            _start = start ?? throw new ArgumentNullException(nameof(start), "Event start can't be null.");
+            SetInterval(start, end);
         }
 
         /// <summary>
@@ -42,7 +42,7 @@ namespace EdlinSoftware.Timeline.Domain
             [DebuggerStepThrough]
             get => _start;
             [DebuggerStepThrough]
-            set
+            private set
             {
                 if(value == null)
                     throw new ArgumentNullException(nameof(value), "Event start can't be null.");
@@ -54,6 +54,35 @@ namespace EdlinSoftware.Timeline.Domain
         /// <summary>
         /// End of event.
         /// </summary>
-        public Date End { get; set; }
+        public Date End { get; private set; }
+
+        /// <summary>
+        /// Sets time interval of this event.
+        /// </summary>
+        /// <param name="start">Start of the event.</param>
+        /// <param name="end">End of the event.</param>
+        public void SetInterval(Date start, Date end = null)
+        {
+            if (start is null) throw new ArgumentNullException(nameof(start), "Event start can't be null.");
+
+            if(end != null && end < start)
+                throw new ArgumentException("Event end should be no less then start.", nameof(end));
+
+            _start = start;
+            End = end;
+        }
+
+        /// <summary>
+        /// Duration of the event.
+        /// </summary>
+        public Duration Duration
+        {
+            get
+            {
+                if (End == null) return Duration.Zero;
+
+                return End - Start;
+            }
+        }
     }
 }
