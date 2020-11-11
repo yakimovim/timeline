6d89763305aaaab3caea9c45268a758fdf9f50be
Ivan Iakimov <edlinsoft@gmail.com>
2020-11-11 11:55:53 +0300

    Hierarchies

---
+++ code/src/Timeline.Domain/Hierarchy.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EdlinSoftware.Timeline.Domain
+{
+    /// <summary>
+    /// Represents a hierarchy.
+    /// </summary>
+    /// <typeparam name="T">Type of hierarchy node content.</typeparam>
+    public class Hierarchy<T> : IEnumerable<HierarchyNode<T>>
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly List<HierarchyNode<T>> _topNodes = new List<HierarchyNode<T>>();
+
+        /// <summary>
+        /// Top nodes of the hierarchy.
+        /// </summary>
+        public IReadOnlyList<HierarchyNode<T>> TopNodes => _topNodes;
+
+        /// <summary>
+        /// Checks if the hierarchy contains node with given id.
+        /// </summary>
+        /// <param name="id">Node id.</param>
+        public bool ContainsNodeWithId(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace", nameof(id));
+            }
+
+            return _topNodes.Any(n => n.Id.Equals(id, StringComparison.OrdinalIgnoreCase))
+                || _topNodes.Any(n => n.ContainsSubNodeWithId(id));
+        }
+
+        /// <summary>
+        /// Gets node with given id.
+        /// </summary>
+        /// <param name="id">Node id.</param>
+        /// <returns>Return null, if there is no node with given id in the hierarchy</returns>
+        public HierarchyNode<T> GetNodeById(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace", nameof(id));
+            }
+
+            return this.FirstOrDefault(n => n.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+        }
+
+        /// <summary>
+        /// Adds new top-level node.
+        /// </summary>
+        /// <param name="id">Node id.</param>
+        /// <param name="content">Node content.</param>
+        /// <returns>True, if the node was added. False, if given id already exists.</returns>
+        public bool AddTopNode(string id, T content)
+        {
+            if (ContainsNodeWithId(id)) return false;
+
+            _topNodes.Add(new HierarchyNode<T>(this, id, content));
+
+            RenumerateNodes();
+
+            return true;
+        }
+
+        internal void RenumerateNodes()
+        {
+            var serviceIndex = 0;
+
+            foreach (var node in _topNodes)
+            {
+                node.Left = serviceIndex++;
+
+                node.RenumerateSubNodes(ref serviceIndex);
+
+                node.Right = serviceIndex++;
+            }
+        }
+
+        public IEnumerator<HierarchyNode<T>> GetEnumerator()
+        {
+            return _topNodes.Concat(_topNodes.SelectMany(n => n)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+
+    /// <summary>
+    /// Represents one node in the hierarchy.
+    /// </summary>
+    /// <typeparam name="T">Type of the content.</typeparam>
+    public sealed class HierarchyNode<T> : IEnumerable<HierarchyNode<T>>
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly List<HierarchyNode<T>> _subNodes = new List<HierarchyNode<T>>();
+        private readonly Hierarchy<T> _hierarchy;
+
+        /// <summary>
+        /// Subnodes of this node.
+        /// </summary>
+        public IReadOnlyList<HierarchyNode<T>> SubNodes => _subNodes;
+
+        public HierarchyNode(Hierarchy<T> hierarchy, string id, T content)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace", nameof(id));
+            }
+
+            _hierarchy = hierarchy ?? throw new ArgumentNullException(nameof(hierarchy));
+            Id = id;
+            Content = content;
+        }
+
+        /// <summary>
+        /// Id of the node.
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        /// Content of the node.
+        /// </summary>
+        public T Content { get; }
+
+        /// <summary>
+        /// Left service index.
+        /// </summary>
+        internal int Left { get; set; }
+
+        /// <summary>
+        /// Right service index.
+        /// </summary>
+        internal int Right { get; set; }
+
+        /// <summary>
+        /// Checks if this node contains subnode with given id.
+        /// </summary>
+        /// <param name="id">Node id.</param>
+        public bool ContainsSubNodeWithId(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace", nameof(id));
+            }
+
+            return _subNodes.Any(n => n.Id.Equals(id, StringComparison.OrdinalIgnoreCase))
+                || _subNodes.Any(n => n.ContainsSubNodeWithId(id));
+        }
+
+        /// <summary>
+        /// Adds new subnode to this node.
+        /// </summary>
+        /// <param name="id">Node id.</param>
+        /// <param name="content">Node content.</param>
+        /// <returns>True, if the node was added. False, if given id already exists in the hierarchy.</returns>
+        public bool AddSubNode(string id, T content)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace", nameof(id));
+            }
+
+            if (_hierarchy.ContainsNodeWithId(id)) return false;
+
+            _subNodes.Add(new HierarchyNode<T>(_hierarchy, id, content));
+
+            _hierarchy.RenumerateNodes();
+
+            return true;
+        }
+
+        internal void RenumerateSubNodes(ref int serviceIndex)
+        {
+            foreach (var node in _subNodes)
+            {
+                node.Left = serviceIndex++;
+
+                node.RenumerateSubNodes(ref serviceIndex);
+
+                node.Right = serviceIndex++;
+            }
+        }
+
+        /// <summary>
+        /// Gets subnode with given id.
+        /// </summary>
+        /// <param name="id">Node id.</param>
+        /// <returns>Return null, if there is no node with given id in the hierarchy</returns>
+        public HierarchyNode<T> GetNodeById(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace", nameof(id));
+            }
+
+            return this.FirstOrDefault(n => n.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public IEnumerator<HierarchyNode<T>> GetEnumerator()
+        {
+            return _subNodes.Concat(_subNodes.SelectMany(n => n)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
---
+++ code/tests/Timeline.Domain.Tests/HierarchyTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture.Xunit2;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EdlinSoftware.Timeline.Domain.Tests
+{
+    public class HierarchyTests
+    {
+        [Theory]
+        [AutoData]
+        public void Empty_hierarchy(string nodeId)
+        {
+            var hierarchy = new Hierarchy<string>();
+
+            hierarchy.TopNodes.Count.ShouldBe(0);
+            hierarchy.ContainsNodeWithId(nodeId).ShouldBeFalse();
+            hierarchy.GetNodeById(nodeId).ShouldBeNull();
+        }
+
+        [Theory]
+        [AutoData]
+        public void Add_top_level_node(string nodeId, string content)
+        {
+            var hierarchy = new Hierarchy<string>();
+
+            hierarchy.AddTopNode(nodeId, content).ShouldBeTrue();
+
+            hierarchy.ContainsNodeWithId(nodeId).ShouldBeTrue();
+
+            hierarchy.TopNodes.Count.ShouldBe(1);
+
+            var node = hierarchy.TopNodes[0];
+
+            node.ShouldNotBeNull();
+            node.Id.ShouldBe(nodeId);
+            node.Content.ShouldBe(content);
+
+            hierarchy.GetNodeById(nodeId).ShouldBeSameAs(node);
+        }
+    }
+}
